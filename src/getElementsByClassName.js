dcbd18c1952eeaf5554166e009d2fdaba777b153
Daniel Esquivel-Reynoso <daniel.esquivelreynoso@gmail.com>
2022-02-22 16:50:32 -0800

    completed getElementByClassName and almost completed stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,61 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, inputElement) {
+//   //edge case for if inputElement was undefined
+//     //make inputElement to body
+//   if (inputElement === undefined) {
+//     inputElement = document.body;
+//   }
+
+//   // console.log('document.body.children, result: ', document.body.children);
+//   var resultArray = [];
+//   // make return array
+//   //first input
+//   //if classname exists
+
+//   // console.log('inputElement.classList before if statement, result: ', inputElement.classList);
+//   if (inputElement.classList.contains(className)) {
+//     // console.log('inputElement.classList inside if statement, result: ', inputElement.classList);
+//     resultArray = resultArray.concat(inputElement);
+//   }
+//     //if so concat element to return array
+//   //does this input include a nested structure
+//     //if so recursively call the function on each value of the nested structure
+//   //start at body, check for children
+//   if (inputElement.childNodes){
+//     for (var i = 0; i < inputElement.childNodes.length; i++){
+//       console.log('childNodes inside recursive statement: ', inputElement.childNodes);
+//       resultArray = resultArray.concat(getElementsByClassName(className, inputElement.childNodes[i]));
+//     }
+//   }
+
+// return resultArray;
+
+  var elements = [];
+
+  var getElementsByClass = function(element) {
+    // base case 1: No more children
+    // if has className
+    // ends recursion
+    if (element.nodeType === 1 && element.classList.contains(className)) {
+      elements.push(element);
+    }
+
+    // recursive case: if children, loop through child array andrun getElementsByClassName
+    // return result array
+    if (element.childNodes.length !== 0) {
+      element.childNodes.forEach(function (item, index) {
+        getElementsByClass(item);
+      })
+    }
+  }
+
+getElementsByClass(document.body);
+return elements;
+
+  // recursive: if children, loop through child array andrun getElementsByClassName
+  // return result array
+
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,47 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  // check for numbers
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+  //check for null
+  if (obj === null) {
+    return 'null';
+  }
+  // check for strings
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  // check for booleans
+  if (typeof obj === 'boolean') {
+    if (obj) {
+      return 'true';
+    }
+    return'false';
+  }
+
+  if (typeof obj === 'object') {
+    if (Array.isArray(obj)) {
+      var stringArray = [];
+      // check for arrays
+      if (obj.length !== 0) {
+        //loop thru array
+        obj.forEach(function(item, index) {
+          stringArray.push(stringifyJSON(item));
+        })
+        stringArray.join(',');
+          //run stringify and store the value in stringArray
+        //.join(',')
+        //return
+        return '[' + stringArray + ']';
+      }
+      return '[' + obj +']';
+    } else { // check for objects implicitly
+      return '{}';
+    }
+    // check for objects
+
+  }
 };
